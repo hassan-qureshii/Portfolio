db7fd397e4d2a26d4c8a9d26a381796223863ee9
Hassan-ureshi <hassanqureshi8886@gmail.com>
2025-10-24 11:15:17 -0700

    changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import LeftItems from '../Components/LeftItems';
-import RightItems from '../Components/RightItems';
+import React from "react";
+import LeftItems from "../Components/LeftItems";
+import RightItems from "../Components/RightItems";
 
 const Home = () => {
   return (
     <div className="bg-background h-screen w-screen grid grid-cols-1 md:grid-cols-[35%_65%]">
-      {/* Left - Side (Fixed, 40% on md+) */}
-      <div className="h-auto md:h-screen md:sticky md:top-0">
+      {/* Left - Side (Normal on mobile, sticky on md+) */}
+      <div className="h-auto md:h-screen md:sticky md:top-0 md:self-start">
         <LeftItems />
       </div>
 
-      {/* Right - Side (Scrollable, 60% on md+) */}
+      {/* Right - Side (Scrollable) */}
       <div className="bg-background pl-5 h-screen overflow-y-auto">
         <RightItems />
       </div>
